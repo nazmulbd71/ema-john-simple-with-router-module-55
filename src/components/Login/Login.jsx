a0fe80df2606426ad680f7d6f91acd2f1aec7875
Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-05-18 23:48:29 +0600

    conatct form create

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <div className='form-parent'>
+                <form>
+                    <div className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name='email' required />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' required />
+                    </div>
+                    <input className='btn-submit' type='submit' value="Login"></input>
+
+                </form>
+            </div>
+            <p className='form-child'>New to Ema-John? <span><Link to="/signup">Create New Account</Link></span></p>
+            <div className='footer-div'><input className='btn-submit-two' type='submit' value="Continue With Google"></input></div>
+
         </div>
     );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCik2ifwb9eGB7RoxSULNTGOTI-NBbHyWA",
+    authDomain: "ema-john-with-firebase-a-6602b.firebaseapp.com",
+    projectId: "ema-john-with-firebase-a-6602b",
+    storageBucket: "ema-john-with-firebase-a-6602b.appspot.com",
+    messagingSenderId: "284044170955",
+    appId: "1:284044170955:web:54178d72f3abf288f62d4e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
