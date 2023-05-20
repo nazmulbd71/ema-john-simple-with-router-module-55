a0c7304fda51137e06ec13f5d14f1c639c11a9f0
Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-05-21 01:16:28 +0600

    final commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,63 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './Login.css'
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import img from '../../../public/google.png'
+import { AuthContext } from '../Providers/AuthProvider';
+
 
 const Login = () => {
+    const [error, setError] = useState('')
+    const [show, setShow] = useState(false)
+    const { signIn } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const location = useLocation()
+    console.log(location)
+    const from = location.state?.from?.pathname || '/';
+
+
+    const handleSignIn = event => {
+        event.preventDefault();
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+        console.log(email, password)
+
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user
+                console.log(loggedUser)
+                form.reset()
+                setError('')
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.log(error)
+                setError(error.message)
+            })
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Login</h2>
             <div className='form-parent'>
-                <form>
+                <form onSubmit={handleSignIn}>
                     <div className="form-control">
                         <label htmlFor="email">Email</label>
                         <input type="email" name='email' required />
                     </div>
                     <div className="form-control">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name='password' required />
+                        <input type={show ? "text" : "password"} name='password' required />
+                        <p onClick={() => setShow(!show)}><small>{
+                            show ? <span>Hide Password</span> : <span>Show Password</span>
+                        }</small></p>
                     </div>
+                    <p className='error'>{error}</p>
                     <input className='btn-submit' type='submit' value="Login"></input>
 
                 </form>
             </div>
             <p className='form-child'>New to Ema-John? <span><Link to="/signup">Create New Account</Link></span></p>
-            <div className='footer-div'><input className='btn-submit-two' type='submit' value="Continue With Google"></input></div>
+            <div className='footer-div'> <button className='btn-submit-two'><img src={img} alt="" /> Continue with google</button></div>
 
         </div>
     );
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './SignUp.css'
+import { Link } from 'react-router-dom';
+import img from '../../../public/google.png'
+import { AuthContext } from '../Providers/AuthProvider';
 
 const SignUp = () => {
+    const [error, setError] = useState('')
+    const { createUser } = useContext(AuthContext)
+    const handleSignUp = event => {
+        event.preventDefault();
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+        const confirm = form.confirm.value
+        console.log(email, password, confirm)
+        createUser(email, password)
+            .then(result => {
+                const loggedUser = result.user
+                console.log(loggedUser)
+            })
+            .catch(error => {
+                console.log(error)
+                setError(error.message)
+            })
+
+        if (password !== confirm) {
+            setError('your password did not match')
+            return;
+        }
+        else if (password.length < 6) {
+            setError('password must be six character')
+            return
+        }
+        else {
+            setError('')
+        }
+    }
     return (
-        <div>
+        <div className='form-container'>
+            <h2 className='form-title'>Sign Up</h2>
+            <div className='form-parent'>
+                <form onSubmit={handleSignUp}>
+                    <div className="form-control">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name='email' required />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' required />
+                    </div>
+
+                    <div className="form-control">
+                        <label htmlFor="confirm">Confirm Password</label>
+                        <input type="password" name='confirm' required />
+                    </div>
+                    <p className='error'>{error}</p>
+                    <input className='btn-submit' type='submit' value="Sign Up"></input>
+
+                </form>
+            </div>
+            <p className='form-child'>Already Have an Account? <span><Link to="/login">Please Log In</Link></span></p>
+            <div className='footer-div'> <button className='btn-submit-two signup-btn'><img src={img} alt="" /> Continue with google</button></div>
 
         </div>
     );
